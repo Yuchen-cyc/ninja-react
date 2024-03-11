4fafa82f9875bdca5173493efd15b4ca837a21df
Yuchen Chen <joyce.yuchen.chen@gmail.com>
2024-03-11 17:27:20 +0100

    the onChange event, control input, submit a form

--- my-first-react-project/react-sandbox/src/components/EventList.js
+++ my-first-react-project/react-sandbox/src/components/EventList.js
@@ -4,7 +4,7 @@ export default function EventList({ events, handleClick }) {
   return (
     <div>
       {events.map((event, index) => (
-        <React.Fragment>
+        <React.Fragment key={event.id}>
           <h2>{index + 1} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>Delete Event</button>
         </React.Fragment>
--- my-first-react-project/react-sandbox/src/components/NewEventForm.js
+++ my-first-react-project/react-sandbox/src/components/NewEventForm.js
@@ -1,16 +1,42 @@
+import { useState } from 'react'
 import './NewEventForm.css'
 
 export default function NewEventForm() {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const event = {
+      title: title,
+      date: date,
+      id: Math.floor(Math.random()*10000)
+    }
+    console.log(event)
+    resetForm()
+  }
+
   return (
     <div>
-      <form className="new-event-form">
+      <form className="new-event-form" onSubmit={handleSubmit}>
         <label>
           <span>Event title:</span>
-          <input type="text" />
+          <input type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
         </label>
         <label>
           <span>Event date</span>
-          <input type="date" />
+          <input type="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+          />
         </label>
         <button>Submit</button>
       </form>
